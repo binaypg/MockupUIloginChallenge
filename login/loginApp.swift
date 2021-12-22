0f2ff6296e209522cd61b499756897ac60d6912d
binay <binay.pg@gmail.com>
2021-12-22 06:08:25 +0530

    Initial Commit

---
+++ login/loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  login
+//
+//  Created by Binay Kumar Sharma on 22/12/21.
+//
+
+import SwiftUI
+
+@main
+struct loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
